e014c71a86594432d88e9add361a6115a8aaa75b
IlyasArrafii <zainularrafii@gmail.cm>
2023-09-18 13:20:52 +0700

    first commit

---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,14 @@
+<template>
+    <Link href="/">Main Page</Link>&nbsp;
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<script>
+import MainLayout from "../../Layouts/MainLayout.vue";
+export default {
+    layout: MainLayout
+}
+</script>
